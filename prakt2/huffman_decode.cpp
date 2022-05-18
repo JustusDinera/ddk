b03ab79f66f374918167354b9e8bb79e6884a684
Dinera <justus@darmsign.al>
2022-05-18 20:00:01 +0200

    .

--- prakt2/huffman_decode.cpp
+++ prakt2/huffman_decode.cpp
@@ -139,10 +139,7 @@ int main(int argc, char const *argv[])
                 {
                     
                 }
-                
             }
-            
-            
         }
     }
     return 0;
